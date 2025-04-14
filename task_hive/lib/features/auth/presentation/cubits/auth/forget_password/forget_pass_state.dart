d11e8d51f424f4bf69937a103738dd8b7db1914e
Mahbub Hasan Talukder <mahbubhasanmht@gmail.com>
2025-04-14 23:30:34 +0600

    Fix/forget-password: Minor fix on user enhancement

--- task_hive/lib/features/auth/presentation/cubits/auth/forget_password/forget_pass_state.dart
+++ task_hive/lib/features/auth/presentation/cubits/auth/forget_password/forget_pass_state.dart
@@ -8,11 +8,17 @@ class ForgetPasswordState extends Equatable {
 class ForgetPasswordSuccess extends ForgetPasswordState {
   final Success success;
   ForgetPasswordSuccess(this.success);
+
+  @override
+  List<Object?> get props => [success];
 }
 
 class ForgetPasswordFailed extends ForgetPasswordState {
   final Failure failure;
   ForgetPasswordFailed(this.failure);
+
+  @override
+  List<Object?> get props => [failure];
 }
 
 class ForgetPasswordLoading extends ForgetPasswordState {}
