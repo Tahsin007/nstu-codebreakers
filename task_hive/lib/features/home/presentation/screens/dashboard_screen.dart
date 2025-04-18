d77cfc89de328957f56b7fb8ca16e600ac49c2e7
Tahsin007 <tahsinahmed.iit@gmail.com>
2025-04-19 04:21:15 +0600

    feature/Dashboard -> Floating Action Button Added

--- task_hive/lib/features/home/presentation/screens/dashboard_screen.dart
+++ task_hive/lib/features/home/presentation/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:task_hive/features/home/presentation/widgets/priority_task_card.dart';
 import 'package:task_hive/features/home/presentation/widgets/upcoming_delivery_card.dart';
 
+import '../../../task_create/presentation/screen/task_create_screen.dart';
 import '../../domain/entities/project_entity.dart';
 import '../../domain/entity/project_info.dart';
 // import 'package:intl/intl.dart';
@@ -60,109 +61,123 @@ class _HomePageState extends State<HomePage> {
     final formattedDate = _formatDate(now);
 
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CreateTaskScreen()),
+            );
+          },
+          backgroundColor: colorScheme.surface,
+          foregroundColor: colorScheme.onSurface,
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Date and notification
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  formattedDate,
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 14,
+                // Date and notification
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      formattedDate,
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 14,
+                      ),
+                    ),
+                    const Icon(
+                      Icons.notifications,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                // Welcome message
+                Text('Welcome Phillip',
+                    style: textTheme.displaySmall
+                        ?.copyWith(color: colorScheme.onSurface)),
+                Text('Have a nice day!', style: textTheme.bodyLarge),
+                const SizedBox(height: 20),
+
+                // Priority tasks section
+                Text('My Priority Task', style: textTheme.headlineMedium),
+                const SizedBox(height: 15),
+
+                // Priority task cards
+                SizedBox(
+                  height: 170,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      priorityTaskCard(
+                        color: Colors.blue,
+                        icon: Icons.design_services,
+                        title: 'UI Design',
+                        days: 10,
+                        progress: 0.8,
+                      ),
+                      priorityTaskCard(
+                        color: Colors.deepPurple,
+                        icon: Icons.code,
+                        title: 'Laravel Task',
+                        days: 20,
+                        progress: 0.3,
+                      ),
+                      priorityTaskCard(
+                        color: Colors.red,
+                        icon: Icons.image,
+                        title: 'Edit Pictures',
+                        days: 5,
+                        progress: 0.6,
+                      ),
+                    ],
                   ),
                 ),
-                const Icon(
-                  Icons.notifications,
-                  color: Colors.blue,
-                  size: 24,
+
+                const SizedBox(height: 20),
+
+                // Delivery tasks section
+                Text(
+                  'Upcoming Deliveries',
+                  style: textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 15),
+
+                // Delivery task cards - Scrollable
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _deliveryTasks.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          UpcomingDeliveryCard(
+                            taskName: _deliveryTasks[index].title,
+                            // timeRange: _deliveryTasks[index].date.toString(),
+                            dueDate: _deliveryTasks[index].date.toString(),
+                            progressPercentage:
+                                _deliveryTasks[index].percentage,
+                            assignees: _deliveryTasks[index].assignee,
+                            priority: _deliveryTasks[index].priority,
+                          ),
+                          const SizedBox(height: 12),
+                        ],
+                      );
+                      // return _buildDeliveryTaskCard(_deliveryTasks[index]);
+                    },
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 20),
-
-            // Welcome message
-            Text('Welcome Phillip',
-                style: textTheme.displaySmall
-                    ?.copyWith(color: colorScheme.onSurface)),
-            Text('Have a nice day!', style: textTheme.bodyLarge),
-            const SizedBox(height: 20),
-
-            // Priority tasks section
-            Text('My Priority Task', style: textTheme.headlineMedium),
-            const SizedBox(height: 15),
-
-            // Priority task cards
-            SizedBox(
-              height: 170,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  priorityTaskCard(
-                    color: Colors.blue,
-                    icon: Icons.design_services,
-                    title: 'UI Design',
-                    days: 10,
-                    progress: 0.8,
-                  ),
-                  priorityTaskCard(
-                    color: Colors.deepPurple,
-                    icon: Icons.code,
-                    title: 'Laravel Task',
-                    days: 20,
-                    progress: 0.3,
-                  ),
-                  priorityTaskCard(
-                    color: Colors.red,
-                    icon: Icons.image,
-                    title: 'Edit Pictures',
-                    days: 5,
-                    progress: 0.6,
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            // Delivery tasks section
-            Text(
-              'Upcoming Deliveries',
-              style: textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 15),
-
-            // Delivery task cards - Scrollable
-            Expanded(
-              child: ListView.builder(
-                itemCount: _deliveryTasks.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      UpcomingDeliveryCard(
-                        taskName: _deliveryTasks[index].title,
-                        // timeRange: _deliveryTasks[index].date.toString(),
-                        dueDate: _deliveryTasks[index].date.toString(),
-                        progressPercentage: _deliveryTasks[index].percentage,
-                        assignees: _deliveryTasks[index].assignee,
-                        priority: _deliveryTasks[index].priority,
-                      ),
-                      const SizedBox(height: 12),
-                    ],
-                  );
-                  // return _buildDeliveryTaskCard(_deliveryTasks[index]);
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
 
